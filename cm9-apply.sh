0dab65a416607705f8e506010b341d2672296df2
Warren Togami <wtogami@gmail.com>
2012-05-01 18:04:43 -1000

    more hashes

--- cm9-apply.sh
+++ cm9-apply.sh
@@ -60,7 +60,7 @@ echo "### Implement Camera GPS borrowed from jt1134 http://review.cyanogenmod.co
 git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_epicmtd refs/changes/02/15002/1 && git cherry-pick FETCH_HEAD
 echo "### Add EpicParts with option to disable capacitive backlights. http://review.cyanogenmod.com/#/c/15421"
 git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_epicmtd refs/changes/21/15421/2 && git cherry-pick FETCH_HEAD
-echo "epicmtd: Use integrated kernel building. http://review.cyanogenmod.com/#/c/14866/"
+echo "### epicmtd: Use integrated kernel building. http://review.cyanogenmod.com/#/c/14866/"
 git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_epicmtd refs/changes/66/14866/6 && git cherry-pick FETCH_HEAD
 cdb
 
@@ -70,7 +70,7 @@ echo "### Patch: CDMA 1 signal bar threshold s/100/105/ to match Samsung"
 http_patch http://asgard.ancl.hawaii.edu/~warren/testonly-cdma-1bar-105-dBm-v3.patch
 git add telephony/java/android/telephony/SignalStrength.java
 git commit -m "DO NOT COMMIT TO GERRIT - need to make into config.xml option for upstream"
-echo "telephony: SamsungRIL cleanup http://review.cyanogenmod.com/#/c/15054/"
+echo "### telephony: SamsungRIL cleanup http://review.cyanogenmod.com/#/c/15054/"
 git fetch http://review.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/54/15054/5 && git cherry-pick FETCH_HEAD
 cdb
 
