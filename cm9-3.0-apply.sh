0ef5934996b92e0a0587f6d6135d5a84424cb589
Warren Togami <wtogami@gmail.com>
2012-05-06 20:10:14 -1000

    Simplifly ROM filename

--- cm9-3.0-apply.sh
+++ cm9-3.0-apply.sh
@@ -83,9 +83,11 @@ echo "### Phone: add voicemail notification setting http://review.cyanogenmod.co
 git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_packages_apps_Phone refs/changes/06/13706/6 && git cherry-pick FETCH_HEAD
 cdb
 
-#repo start auto packages/apps/Camera
-#cdv packages/apps/Camera
-#cdb
+repo start auto vendor/cm
+cdv vendor/cm
+echo "### Simplifly ROM filename, add CM_EXPERIMENTAL, datestamp UNOFFICIAL, remove some dead code. http://review.cyanogenmod.com/#/c/15662/"
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_vendor_cm refs/changes/62/15662/1 && git cherry-pick FETCH_HEAD
+cdb
 
 ##### SUCCESS ####
 SUCCESS=true
