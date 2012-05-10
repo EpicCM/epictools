f61bf321b7e19dcb8444f0ad9e45d127074df90b
Jeremy Blankenship <jerbla88@gmail.com>
2012-05-10 00:21:47 -0500

    Again...

--- aokp-apply.sh
+++ aokp-apply.sh
@@ -69,28 +69,28 @@ repo start auto device/samsung/epicmtd
 cdv device/samsung/epicmtd
 echo "### epicmtd: enable enable_vmnotif_option http://review.cyanogenmod.com/#change,13739"
 git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_device_samsung_epicmtd refs/changes/39/13739/1 && git cherry-pick FETCH_HEAD
-echo "### Implement Camera GPS borrowed from jt1134 http://review.cyanogenmod.com/#/c/15002/"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_epicmtd refs/changes/02/15002/1 && git cherry-pick FETCH_HEAD
+#echo "### Add EpicParts with option to disable capacitive backlights. http://review.cyanogenmod.com/#/c/15421"
+#git fetch http://r.cyanogenmod.com/CyanogenMod/android_device_samsung_epicmtd refs/changes/21/15421/3 && git cherry-pick FETCH_HEAD
+#echo "### epicmtd: Read bt mac address from ril and setprop to our own BDADDR PATH http://review.cyanogenmod.com/#/c/15603/"
+#git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_epicmtd refs/changes/03/15603/7 && git cherry-pick FETCH_HEAD
 cdb
 
 repo start auto frameworks/base 
 cdv frameworks/base
-#echo "### Patch: CDMA 1 signal bar threshold s/100/105/ to match Samsung"
-#http_patch http://asgard.ancl.hawaii.edu/~warren/testonly-cdma-1bar-105-dBm-v3.patch
-#git add telephony/java/android/telephony/SignalStrength.java
-#git commit -m "DO NOT COMMIT TO GERRIT - need to make into config.xml option for upstream"
-echo "telephony: SamsungRIL cleanup http://review.cyanogenmod.com/#/c/15054/"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/54/15054/5 && git cherry-pick FETCH_HEAD
+echo "### telephony: CDMA signal bar threshold s/100/105/ to match Samsung's behavior (DO NOT COMMIT) http://review.cyanogenmod.com/#/c/15580/"
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/80/15580/5 && git cherry-pick FETCH_HEAD
+#echo "### Additional fixes for button/keyboard backlight auto-brightness. http://review.cyanogenmod.com/15726"
+#git fetch http://r.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/26/15726/1 && git cherry-pick FETCH_HEAD
 cdb
 
 repo start auto kernel/samsung/victory
 cdv kernel/samsung/victory
-#echo "### Epicmtd: reduce framebuffer NR size to 2 and frees 7mb to userspace http://review.cyanogenmod.com/#change,14386"
-#git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_kernel_samsung_victory refs/changes/86/14386/1 && git cherry-pick FETCH_HEAD
-echo "### Test with CONFIG_FB_S3C_NR_BUFFERS=6 since =2 was rejected (read above gerrit)"
-http_patch http://asgard.ancl.hawaii.edu/~warren/test-CONFIG_FB_S3C_NR_BUFFERS-6.patch
-git add Kernel/arch/arm/configs/cyanogenmod_epicmtd_defconfig
-git commit -m "DO NOT COMMIT TO GERRIT - test CONFIG_FB_S3C_NR_BUFFERS=6"
+#echo "### Omnibus fixes for cypress-touchkey. http://review.cyanogenmod.com/15654"
+#git fetch http://r.cyanogenmod.com/CyanogenMod/android_kernel_samsung_victory refs/changes/54/15654/2 && git cherry-pick FETCH_HEAD
+#echo "### Add sysfs control for capacitive backlights. http://review.cyanogenmod.com/#/c/15420"
+#git fetch http://r.cyanogenmod.com/CyanogenMod/android_kernel_samsung_victory refs/changes/20/15420/6 && git cherry-pick FETCH_HEAD
+#echo "### On slide open, set keyboard backlight to configured state, not always on. http://review.cyanogenmod.com/15750"
+#git fetch http://r.cyanogenmod.com/CyanogenMod/android_kernel_samsung_victory refs/changes/50/15750/1 && git cherry-pick FETCH_HEAD
 cdb
 
 repo start auto packages/apps/Phone
