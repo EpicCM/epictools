cb08e1952f9ee7b26f76b0de56117061b434bec0
Mike Kasick <mike@kasick.org>
2012-05-15 16:01:45 -0400

    Remove merged beta3 gerrits.
    Change-Id: Ic4b687841395fcc97ae2bbe2ff7b711cda76890b

--- cm9-apply.sh
+++ cm9-apply.sh
@@ -62,8 +62,6 @@ repo start auto device/samsung/epicmtd
 cdv device/samsung/epicmtd
 echo "### epicmtd: enable enable_vmnotif_option http://review.cyanogenmod.com/#change,13739"
 git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_device_samsung_epicmtd refs/changes/39/13739/1 && git cherry-pick FETCH_HEAD
-echo "### Add EpicParts with option to disable capacitive backlights. http://review.cyanogenmod.com/#/c/15421"
-git fetch http://r.cyanogenmod.com/CyanogenMod/android_device_samsung_epicmtd refs/changes/21/15421/4 && git cherry-pick FETCH_HEAD
 echo "### epicmtd: Read bt mac address from ril and setprop to our own BDADDR PATH http://review.cyanogenmod.com/#/c/15603/"
 git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_epicmtd refs/changes/03/15603/7 && git cherry-pick FETCH_HEAD
 echo "### epicmtd: Enable CWM repeatable keys http://review.cyanogenmod.com/#/c/16037/"
@@ -78,16 +76,9 @@ echo "### telephony: CDMA signal bar threshold s/100/105/ to match Samsung's beh
 git fetch http://review.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/80/15580/5 && git cherry-pick FETCH_HEAD
 cdb
 
-repo start auto kernel/samsung/victory
-cdv kernel/samsung/victory
-echo "### Omnibus fixes for cypress-touchkey. http://review.cyanogenmod.com/15654"
-git fetch http://r.cyanogenmod.com/CyanogenMod/android_kernel_samsung_victory refs/changes/54/15654/4 && git cherry-pick FETCH_HEAD
-echo "### Add sysfs control for capacitive backlights. http://review.cyanogenmod.com/#/c/15420"
-git fetch http://r.cyanogenmod.com/CyanogenMod/android_kernel_samsung_victory refs/changes/20/15420/8 && git cherry-pick FETCH_HEAD
-echo "### On slide open, set keyboard backlight to configured state, not always on. http://review.cyanogenmod.com/15750"
-git fetch http://r.cyanogenmod.com/CyanogenMod/android_kernel_samsung_victory refs/changes/50/15750/1 && git cherry-pick FETCH_HEAD
-cdb
-
+#repo start auto kernel/samsung/victory
+#cdv kernel/samsung/victory
+#cdb
 
 repo start auto packages/apps/Phone
 cdv packages/apps/Phone
