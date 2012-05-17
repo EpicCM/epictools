cb4a172652298c9a8ae8a3ed0f01bba77c07b04f
Jeremy Blankenship <jerbla88@gmail.com>
2012-05-16 21:11:40 -0700

    Cleanup on aisle 4

--- aokp-apply.sh
+++ aokp-apply.sh
@@ -57,28 +57,18 @@ repo start auto device/samsung/epicmtd
 cdv device/samsung/epicmtd
 echo "### epicmtd: enable enable_vmnotif_option http://review.cyanogenmod.com/#change,13739"
 git fetch http://review.cyanogenmod.com/p/CyanogenMod/android_device_samsung_epicmtd refs/changes/39/13739/1 && git cherry-pick FETCH_HEAD
-#echo "### Add EpicParts with option to disable capacitive backlights. http://review.cyanogenmod.com/#/c/15421"
-#git fetch http://r.cyanogenmod.com/CyanogenMod/android_device_samsung_epicmtd refs/changes/21/15421/3 && git cherry-pick FETCH_HEAD
-#echo "### epicmtd: Read bt mac address from ril and setprop to our own BDADDR PATH http://review.cyanogenmod.com/#/c/15603/"
-#git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_epicmtd refs/changes/03/15603/7 && git cherry-pick FETCH_HEAD
 cdb
 
 repo start auto frameworks/base 
 cdv frameworks/base
 #echo "### telephony: CDMA signal bar threshold s/100/105/ to match Samsung's behavior (DO NOT COMMIT) http://review.cyanogenmod.com/#/c/15580/"
 #git fetch http://review.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/80/15580/5 && git cherry-pick FETCH_HEAD
-#echo "### Additional fixes for button/keyboard backlight auto-brightness. http://review.cyanogenmod.com/15726"
-#git fetch http://r.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/26/15726/1 && git cherry-pick FETCH_HEAD
+echo "### Additional fixes for button/keyboard backlight auto-brightness. http://review.cyanogenmod.com/15726"
+git fetch http://r.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/26/15726/1 && git cherry-pick FETCH_HEAD
 cdb
 
 repo start auto kernel/samsung/victory
 cdv kernel/samsung/victory
-#echo "### Omnibus fixes for cypress-touchkey. http://review.cyanogenmod.com/15654"
-#git fetch http://r.cyanogenmod.com/CyanogenMod/android_kernel_samsung_victory refs/changes/54/15654/2 && git cherry-pick FETCH_HEAD
-#echo "### Add sysfs control for capacitive backlights. http://review.cyanogenmod.com/#/c/15420"
-#git fetch http://r.cyanogenmod.com/CyanogenMod/android_kernel_samsung_victory refs/changes/20/15420/6 && git cherry-pick FETCH_HEAD
-#echo "### On slide open, set keyboard backlight to configured state, not always on. http://review.cyanogenmod.com/15750"
-#git fetch http://r.cyanogenmod.com/CyanogenMod/android_kernel_samsung_victory refs/changes/50/15750/1 && git cherry-pick FETCH_HEAD
 cdb
 
 repo start auto packages/apps/Phone
